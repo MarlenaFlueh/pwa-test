558b760a6f1fa105bc7ddf70eea2cf6f391c8b63
Marlena Flüh <marlena.flueh@white-paper-media.de>
2019-07-04 13:38:01 +0200

    remove /dist from .gitignore

---
+++ dist/precache-manifest.c0102bcc9c2a4b717dad517af1309968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ce808654002c0146fe16",
+    "url": "/js/chunk-vendors.fe2ef355.js"
+  },
+  {
+    "revision": "98df5511715029fc00e2",
+    "url": "/js/app.7beed676.js"
+  },
+  {
+    "revision": "4f9a9e248f8f5126c3cd",
+    "url": "/js/about.acc2b994.js"
+  },
+  {
+    "revision": "70edce2076e488c863021f1698dd7b2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "98df5511715029fc00e2",
+    "url": "/css/app.dde1df84.css"
+  }
+];
